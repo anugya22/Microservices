1c6cb3fcde0691d21aa065f93da0a908294c0e5a
anugya22 <141493025+anugya22@users.noreply.github.com>
2025-06-19 11:53:42 +0530

    Update device.module.ts

--- device-api src/device/device.module.ts
+++ device-api src/device/device.module.ts
@@ -3,12 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DevicesService } from './device.service';
 import { DevicesController } from './device.controller';
 import { Device, DeviceSchema } from './device.schema';
-import { RabbitMQModule } from '../rabbitmq/rabbitmq.module'; // ✅ Import this module
+import { RabbitMQModule } from '../rabbitmq/rabbitmq.module'; 
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Device.name, schema: DeviceSchema }]),
-    RabbitMQModule, // ✅ Add RabbitMQModule so service can be injected
+    RabbitMQModule, 
   ],
   providers: [DevicesService],
   controllers: [DevicesController],
