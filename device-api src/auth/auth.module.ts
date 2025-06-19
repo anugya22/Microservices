a3531ac67563653ee81afa58835eff87bdd1cd17
anugya22 <141493025+anugya22@users.noreply.github.com>
2025-06-19 11:51:54 +0530

    Update auth.module.ts

--- device-api src/auth/auth.module.ts
+++ device-api src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule, // 👈 Import it here
+    ConfigModule, 
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
