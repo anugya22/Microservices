9659f97cf187e5034f6d37bddacdde6a26d858db
anugya22 <141493025+anugya22@users.noreply.github.com>
2025-06-30 20:06:46 +0530

    Update app.module.ts

--- device-api src/app.module.ts
+++ device-api src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { DevicesModule } from './device/device.module';
 import { BulkModule } from './bulk/bulk.module';
-import { RabbitMQModule } from './rabbitmq/rabbitmq.module'; 
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+import { SearchModule } from './search/search.module'; 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+
+   
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -16,10 +20,15 @@ import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
       }),
       inject: [ConfigService],
     }),
+
+   
     AuthModule,
     DevicesModule,
     BulkModule,
-    RabbitMQModule, 
+    RabbitMQModule,
+
+    
+    SearchModule,
   ],
 })
 export class AppModule {}
